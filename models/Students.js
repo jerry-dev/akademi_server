653afc15554098ef2dd07a0ad52da9bd9ec3383c
JerryD <jerry.webdev@gmail.com>
2022-03-28 13:17:36 -0400

    FEATURE ADD: Add The “find” and “findBy” Methods WIthin The Students Model

---
+++ models/Students.js
@@ -0,0 +1,44 @@
+import config from 'config';
+import mongodb from 'mongodb';
+
+const MongoClient = mongodb.MongoClient;
+
+const dbURL = `${config.get('database.host')}:${config.get('database.port')}`;
+const database = config.get('database.dbName');
+
+export default class Students {
+    async find() {
+        let client;
+
+        try {
+            client = await MongoClient.connect(dbURL, { useUnifiedTopology: true });
+            console.info(`SUCCESSFULLY CONNECTED TO THE ${database}`);
+            const students = client.db(database).collection('students');
+            return await students.find().toArray();
+        } catch (error) {
+            console.error(`CONNECTION FAILED: ${error.message}`);
+        } finally {
+            if (client) {
+                client.close();
+            }
+        }
+    }
+
+    async findBy(criteria) {
+        let query = [{$match: criteria}];
+        let client;
+
+        try {
+            client = await MongoClient.connect(dbURL, { useUnifiedTopology: true });
+            console.info(`SUCCESSFULLY CONNECTED TO THE ${database}`);
+            const students = client.db(database).collection('students');
+            return await students.find(query).toArray();
+        } catch (error) {
+            console.info(`SUCCESSFULLY CONNECTED TO THE ${database}`);
+        } finally {
+            if (client) {
+                client.close();
+            }
+        }
+    }
+}
