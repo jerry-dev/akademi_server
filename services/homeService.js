0e9be81a6a1dc9a84a960226cd20c508e26b2ee5
JerryD <jerry.webdev@gmail.com>
2022-03-29 08:53:42 -0400

    REFACTOR + ADD: Remove teachers.js, messages.js, foods.js, events.js From services Directory And Make Progress On homeService.

--- services/homeService.js
+++ services/homeService.js
@@ -2,27 +2,39 @@ import StudentModel from '../models/Student.js';
 import EventModel from '../models/Events.js';
 import FoodModel from '../models/Food.js';
 
-const studentModel = new StudentModel()
+const studentModel = new StudentModel();
+const foodModel = new FoodModel();
 
 class HomeService {
-    constructor(studentModel) {
+    constructor(studentModel, foodModel) {
         this.studentModel = studentModel;
+        this.foodModel = foodModel;
     }
 
     async getHomeData() {
         const homeData = {
-            overview: {},
+            overview: {
+                studentsCount: 0,
+                teachersCount: 0,
+                eventsCount: 0,
+                foodsCount: 0
+            },
             unpaidTuition: []
         };
 
         try {
             const students = await this.studentModel.find();
+            const foods = await this.foodModel.find();
+
+            homeData.overview.studentsCount = students.length;
+            homeData.overview.foodsCount = foods.length;
+            return homeData;
         } catch (error) {
             console.error(`Error:`, error.message);
         }
     }
 }
 
-const homeService = new HomeService(studentModel);
+const homeService = new HomeService(studentModel, foodModel);
 
 export default homeService;
