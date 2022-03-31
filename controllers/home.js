feaf1f2c192fff824d6724799e3f300cc65addf2
JerryD <jerry.webdev@gmail.com>
2022-03-31 18:30:03 -0400

    REFACTOR: Alter How The Response Headers Are Sent

--- controllers/home.js
+++ controllers/home.js
@@ -4,13 +4,11 @@ class HomeController {
     async getHomeData(req, res) {
         try {
             const homeData = await homeService.getHomeData();
-            return res
-                .header("Access-Control-Allow-Origin", "*")
-                .header(
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header(
                     "Access-Control-Allow-Headers",
-                    "Origin, X-Requested-With, Content-Type, Accept")
-                .status(200)
-                .send(homeData);
+                    "Origin, X-Requested-With, Content-Type, Accept");
+            res.status(200).send(homeData);
         } catch (error) {
             return res.status(404).send(send.message);
         }
